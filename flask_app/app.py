7747d87f7c64987bdc9bfff49e86487175411312
JanKaczmarski <krrrakan@gmail.com>
2024-07-09 23:08:13 +0200

    Revert changes

--- flask_app/app.py
+++ flask_app/app.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 from werkzeug.security import generate_password_hash, check_password_hash
 
-
+#
 
 
 #read database credentials from enviroment
@@ -60,20 +60,10 @@ class Restaurant(db.Model):
 
     def __repr__(self):
         return f'<Restaurant {self.name}>'
-    
-class Menu(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
-    dish_name = db.Column(db.String(255), nullable=False)  # Nazwa pola dish_name
-    price = db.Column(db.Numeric(precision=10, scale=2), nullable=False)
-
-    def __repr__(self):
-        return f'<Menu {self.dish_name} - {self.price}>'
 
 @app.route('/')
 def home():
     return render_template('home.html')
-
 @app.route ('/users')
 def users():
     all_users = User.query.all()
@@ -97,14 +87,12 @@ def edit_user(user_id):
     return render_template('edit_user.html', user=user)
 
 
-@app.route('/rest', methods=['GET'])
+@app.route('/rest')
 def rest():
     all_restaurants = Restaurant.query.all()
-    restaurants_list = [{'id': restaurant.id, 'name': restaurant.name, 'address': restaurant.address, 'phone': restaurant.phone} 
-                        for restaurant in all_restaurants]
-    return {'restaurants': restaurants_list}
+    return render_template('rest.html', restaurants=all_restaurants)
 
-@app.route('/register', methods=['POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         print(request.form)
@@ -144,49 +132,10 @@ def rest_add():
         new_restaurant = Restaurant(name=name, address=address, phone=phone)
         db.session.add(new_restaurant)
         db.session.commit()
+        
         print('Restaurant added!')
-        return redirect(url_for('menu_add', restaurant_id=new_restaurant.id))
     
     return render_template('rest_add.html')
-
-feature/insert-restaurant-option
-@app.route('/menu_add/<int:restaurant_id>', methods=['GET', 'POST'])
-def menu_add(restaurant_id):
-    restaurant = Restaurant.query.filter_by(id=restaurant_id).first()
-
-    if request.method == 'POST':
-        dish_name = request.form.get('dish_name')
-        price = request.form.get('price')
-
-        print(f"Received Data: dish_name={dish_name}, price={price}")
-
-        if not dish_name or not price:
-            print('All fields are required!')
-            return redirect(request.url)
-        
-        try:
-            price = float(price)
-        except ValueError:
-            print('Price must be a number!')
-            return redirect(request.url)
-        
-        new_menu_item = Menu(restaurant_id=restaurant_id, dish_name=dish_name, price=price)
-        db.session.add(new_menu_item)
-        db.session.commit()
-        print('Menu item added!')
-        return redirect(url_for('menu_add', restaurant_id=restaurant_id))
-
-    return render_template('menu_add.html', restaurant=restaurant)
-
-@app.route('/menu', methods=['GET'])
-def menu():
-    all_menu_items = Menu.query.all()
-    menu_list = [
-        {'id': item.id, 'restaurant_id': item.restaurant_id, 'dish_name': item.dish_name, 'price': float(item.price)} 
-        for item in all_menu_items
-    ]
-    return {'menu_items': menu_list}
-
 
 # Logout works by deleting the cookie that stores the user login status
 @app.route('/logout', methods=['GET', 'POST'])
@@ -218,8 +167,7 @@ def login():
         return response
         
     return render_template('login.html')
-main
 
 # If you re not using docker please uncomment the line below 
-if __name__ == "__main__":
-   app.run(port=5001)
+#if __name__ == "__main__":
+#    app.run(port=5001)
