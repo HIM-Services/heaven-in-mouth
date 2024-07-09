caa630d4e52ee469f7869a37fcd833773c121cbf
Mateusz Matyskiel <346677@uwr.edu.pl>
2024-07-09 12:19:20 +0200

    feat(flask_app): ➕ Add restaurant model

--- flask_app/app.py
+++ flask_app/app.py
@@ -28,6 +28,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+class Restaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name =  db.Column(db.String(200), nullable=False)
+    adress = db.Column(db.String(255), nullable=False)
+    phone = db.Column(db.integer(20), nullable=False)
+
+    def __repr__(self):
+        return f'<Restaurant {self.name}>'
+
 @app.route('/')
 def home():
     return render_template('home.html')
