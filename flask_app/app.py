45a8e14972329330e8f698da0dfb3886bfb138a3
Mateusz Matyskiel <346677@uwr.edu.pl>
2024-07-09 22:38:44 +0200

    dummy

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,13 +1,10 @@
 import os
-from flask import Flask, render_template, redirect, flash, url_for, session, make_response
-from flask_wtf.csrf import generate_csrf
-from flask import request
+from flask import Flask, render_template, redirect, request, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 from werkzeug.security import generate_password_hash, check_password_hash
 
-
-
+app = Flask(__name__)
 
 #read database credentials from enviroment
 def get_env_variable(name):
@@ -20,20 +17,12 @@ def get_env_variable(name):
 # dotenv is used to read the .env file and set the environment variables
 load_dotenv()
 
-SECRET_KEY = get_env_variable("SECRET_KEY")
+
 POSTGRES_URL = get_env_variable("POSTGRES_URL")
 POSTGRES_USER = get_env_variable("POSTGRES_USER")
 POSTGRES_PW = get_env_variable("POSTGRES_PW")
 POSTGRES_DB = get_env_variable("POSTGRES_DB")
-
-
 DB_URL = f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PW}@{POSTGRES_URL}/{POSTGRES_DB}'
-
-
-# SECRET KEY is used to sign the session cookie and other security related stuff #
-app = Flask(__name__)
-app.secret_key='secret_key'
-
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
@@ -42,7 +31,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
     user_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
@@ -50,7 +39,7 @@ class User(db.Model):
     phone = db.Column(db.String(20), nullable=False)
     
     def __repr__(self):
-        return f'<user {self.name}>'
+        return f'<User {self.name}>'
 
 class Restaurant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -78,24 +67,6 @@ def home():
 def users():
     all_users = User.query.all()
     return render_template('users.html', users=all_users)
-    
-@app.route ('/delete_user/<int:user_id>', methods=['GET', 'POST'])
-def delete_user(user_id):
-    user = User.query.get(user_id)
-    db.session.delete(user)
-    db.session.commit()
-    return redirect(url_for('users'))
-
-@app.route('/edit_user/<int:user_id>', methods=['GET', 'POST'])
-def edit_user(user_id):
-    user = User.query.get(user_id)
-    if request.method == 'POST':
-        user.name = request.form.get('name')
-        user.phone = request.form.get('phone')
-        db.session.commit()
-        return redirect(url_for('users'))
-    return render_template('edit_user.html', user=user)
-
 
 @app.route('/rest', methods=['GET'])
 def rest():
@@ -107,11 +78,10 @@ def rest():
 @app.route('/register', methods=['POST'])
 def register():
     if request.method == 'POST':
-        print(request.form)
+        print(request.form)  # This will print the form data to the console
         name = request.form.get('name')  
         email = request.form.get('email')
         phone = request.form.get('phone')
-        
         plain_text_password = request.form.get('password')
 
         if not plain_text_password: 
@@ -123,7 +93,7 @@ def register():
         db.session.add(new_user)
         db.session.commit()
         
-        return redirect(url_for('users'))
+        return redirect(url_for('login'))
     return render_template('register.html')
 
 @app.route('/rest_add', methods=['GET', 'POST'])
@@ -149,7 +119,6 @@ def rest_add():
     
     return render_template('rest_add.html')
 
-feature/insert-restaurant-option
 @app.route('/menu_add/<int:restaurant_id>', methods=['GET', 'POST'])
 def menu_add(restaurant_id):
     restaurant = Restaurant.query.filter_by(id=restaurant_id).first()
@@ -188,38 +157,6 @@ def menu():
     return {'menu_items': menu_list}
 
 
-# Logout works by deleting the cookie that stores the user login status
-@app.route('/logout', methods=['GET', 'POST'])
-def logout():
-    response = make_response(redirect(url_for('home')))
-    response.set_cookie('login', '', expires=0)
-    session.pop('csrf_token', None)
-    return redirect(url_for('home'))
-    
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
-        
-        user = User.query.filter_by(email=email).first()
-        
-        if not user:
-            return 'User not found'
-        
-        if not check_password_hash(user.password, password):
-            return 'Incorrect password'
-        
-        session['csrf_token'] = generate_csrf()
-        
-        response = make_response(redirect(url_for('home')))
-        response.set_cookie('login', 'true', httponly=True)
-        
-        return response
-        
-    return render_template('login.html')
-main
-
 # If you re not using docker please uncomment the line below 
-if __name__ == "__main__":
-   app.run(port=5001)
+#if __name__ == "__main__":
+#   app.run(port=5001)
